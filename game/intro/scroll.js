b60ebbf20e03468b5ce0eb3958980a3aedf95e2c
Travis Adams <travistca@gmail.com>
2025-10-29 20:25:46 -0400

    Automatically scroll through animation

---
+++ game/intro/scroll.js
@@ -0,0 +1,37 @@
+const SCROLL_DISTANCE_X = 0;
+const SCROLL_DISTANCE_Y = 14;
+const SCROLL_DELAY = 10;
+let autoScrolling = false;
+let autoScrollIntervalId;
+let bottomReached = false;
+
+function startAutoScroll() {
+  autoScrolling = true;
+  autoScrollIntervalId = setInterval(
+    window.scrollBy,
+    SCROLL_DELAY,
+    SCROLL_DISTANCE_X,
+    SCROLL_DISTANCE_Y,
+  );
+}
+
+function stopAutoScroll() {
+  clearInterval(autoScrollIntervalId);
+  autoScrolling = false;
+}
+
+function manageAutoScroll() {
+  if (!bottomReached) {
+    if (!autoScrolling) {
+      startAutoScroll();
+    } else if (
+      window.scrollY >=
+      document.body.scrollHeight - window.innerHeight
+    ) {
+      stopAutoScroll();
+      bottomReached = true;
+    }
+  }
+}
+
+window.addEventListener("scroll", manageAutoScroll);
