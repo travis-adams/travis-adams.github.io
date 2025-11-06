afbf88dd27d097a709260c4517042c80b528f48d
Travis Adams <travistca@gmail.com>
2025-11-05 20:42:15 -0500

    Open dev server to local network
    (for live testing on mobile)

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import { defineConfig } from "vite";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  server: {
+    host: true,
+  },
   build: {
     rollupOptions: {
       input: {
